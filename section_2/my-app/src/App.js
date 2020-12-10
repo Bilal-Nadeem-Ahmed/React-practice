2a86dafa151e3bf25ff0caf04eef73e195fc17aa
Bilal-Nadeem-Ahmed <61297097+Megamind44@users.noreply.github.com>
2020-12-10 01:21:34 +0000

    section 2

---
+++ section_2/my-app/src/App.js
@@ -0,0 +1,14 @@
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ayayay</h1>
+      <p>Welcome to d ting</p>
+      <p>set d group rout</p>
+    </div>
+  );
+}
+
+export default App;
